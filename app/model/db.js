8cdabc9578ce4fd2d5a90719d711053b583828d9
thomas1204 <shayamthomas.official@gmail.com>
2020-03-17 11:33:21 +0530

    DB functions are changed to support async/await.

--- app/model/db.js
+++ app/model/db.js
@@ -4,18 +4,12 @@ const MongoDb = require('./mongoDb');
  * Returns a single document from a collection based on query
  * @param collection - name of the collection
  * @param query - conditions to filter the data
- * @param callback
  * @constructor
  */
-const FIND = (collection, query, callback) => {
+const FIND = async (collection, query) => {
 	const db = MongoDb.getDb();
-	db.collection(collection).findOne(query, {}, function (error, result) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, result);
-		}
-	})
+	const DATA = await db.collection(collection).findOne(query, {});
+	return DATA;
 };
 
 
@@ -36,18 +30,12 @@ const GET = async (collection, query) => {
  * Inserts a document to a collection
  * @param collection
  * @param doc
- * @param callback
  * @constructor
  */
-const INSERT = (collection, doc, callback) => {
+const INSERT = async (collection, doc) => {
 	const db = MongoDb.getDb();
-	db.collection(collection).insertOne(doc, {}, function (error, result) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, result.ops[0]);
-		}
-	})
+	const DATA = await db.collection(collection).insertOne(doc, {});
+	return DATA.ops[0];
 };
 
 
@@ -55,18 +43,12 @@ const INSERT = (collection, doc, callback) => {
  * Delets a document from a collection
  * @param collection
  * @param query
- * @param callback
  * @constructor
  */
-const DELETE = (collection, query, callback) => {
+const DELETE = async (collection, query) => {
 	const db = MongoDb.getDb();
-	db.collection(collection).deleteOne(query, {}, function (error, result) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, result);
-		}
-	})
+	const DATA = await db.collection(collection).deleteOne(query, {});
+	return DATA;
 };
 
 
@@ -75,18 +57,12 @@ const DELETE = (collection, query, callback) => {
  * @param collection
  * @param query
  * @param doc
- * @param callback
  * @constructor
  */
-const UPDATE = (collection, query, doc, callback) => {
+const UPDATE = async (collection, query, doc) => {
 	const db = MongoDb.getDb();
-	db.collection(collection).findOneAndUpdate(query, doc, {returnOriginal: false}, function (error, result) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, result.value);
-		}
-	})
+	const DATA = await db.collection(collection).findOneAndUpdate(query, doc, {returnOriginal: false});
+	return DATA.value
 };
 
 
@@ -94,18 +70,12 @@ const UPDATE = (collection, query, doc, callback) => {
  * Returns the count of documents in a collection based on query
  * @param collection
  * @param query
- * @param callback
  * @constructor
  */
-const COUNT = (collection, query, callback) => {
+const COUNT = async (collection, query) => {
 	const db = MongoDb.getDb();
-	db.collection(collection).countDocuments(query, {}, function (error, result) {
-		if (error) {
-			callback(error, null);
-		} else {
-			callback(null, result);
-		}
-	})
+	const COUNT = await db.collection(collection).countDocuments(query, {});
+	return COUNT;
 };
 
 
